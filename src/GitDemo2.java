6b1ef90e6399079945709c2b07607d0f933e9929
Kavitha Periyakaruppan <kperiyakarup@csc.com>
2019-05-02 12:39:49 +0100

    Added suresh and balaji as well

--- src/GitDemo2.java
+++ src/GitDemo2.java
@@ -4,6 +4,8 @@ public class GitDemo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Divya");
+		System.out.println("Balaji");
+		System.out.println("Suresh");
 
 	}
 
